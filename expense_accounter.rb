58f229f8fc97967ff2c6b3f91d26ff02309f839e
Jess <jbattle2718@gmail.com>
2016-12-12 09:15:08 -0500

    set up dependencies; started setting up db

--- expense_accounter.rb
+++ expense_accounter.rb
@@ -1,3 +1,9 @@
 # What are the resources that will be modified?
 # What pages will the user see?
 # Come up with some basic use cases
+require 'sinatra'
+require 'data_mapper'
+
+configure do
+	DataMapper.setup(:default, 'put db connection here')
+end
